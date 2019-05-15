2e67d48899e3d4d573867178ca6cef51eaae62eb
Miri <mirimarkovitzbl@gmail.com>
2019-05-14 19:21:08 -0700

    add to gitignore and create gulp file and json files.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// Require Gulp first!
+var gulp = require("gulp");
+// This is a very basic Gulp task,
+// with a name and some code to run
+// when this task is called:
+gulp.task("default", function (done) {
+    console.log("Hello world");
+    done();
+});
