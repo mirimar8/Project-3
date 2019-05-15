d514f5af33e3680825c58e5d777c69dac289dd0b
Miri <mirimarkovitzbl@gmail.com>
2019-05-15 10:07:27 -0700

    create a readme file, add gulp with new dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,42 @@
-// Require Gulp first!
-var gulp = require("gulp");
-// This is a very basic Gulp task,
-// with a name and some code to run
-// when this task is called:
-gulp.task("default", function (done) {
-    console.log("Hello world");
-    done();
+const gulp = require("gulp"); // Load Gulp!
+// Now that we've installed the terser package we can require it:
+const terser = require("gulp-terser"),
+    rename = require("gulp-rename");
+const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
+
+// Static server
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
+gulp.task("scripts", function () {
+    return gulp
+        .src("./js/*.js") // What files do we want gulp to consume?
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .pipe(terser()) // Call the terser function on these files
+        .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
+        .pipe(gulp.dest("./build/js")); // Where do we put the result?
+});
+
+
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
+gulp.task("watch", function () {
+    gulp.watch("./js/*.js", gulp.series("scripts"));
+});
+
+
+
+gulp.task('default', gulp.parallel('watch', "browser-sync"));
+
+
+
