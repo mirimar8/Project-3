ef680db15c025f7f6ffc21821bf55cef05b066e7
Miri <mirimarkovitzbl@gmail.com>
2019-05-27 12:18:25 -0700

    add sass files and install sass and related dependencies with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const gulp = require("gulp"); // Load Gulp!
 // Now that we've installed the terser package we can require it:
-const terser = require("gulp-terser"),
-    rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const eslint = require('gulp-eslint');
+const prettyError = require('gulp-prettyerror');
+
+//Sass required element -  https://www.npmjs.com/package/gulp-sass
+const sass = require('gulp-sass');
+//Minify our css -  https://www.npmjs.com/package/gulp-uglifycss
+const uglifycss = require('gulp-uglifycss');
+
 
 // Static server
 gulp.task('browser-sync', function () {
@@ -24,6 +32,26 @@ gulp.task("scripts", function () {
         .pipe(gulp.dest("./build/js")); // Where do we put the result?
 });
 
+// Create Sass task for compiling sass
+gulp.task('sass', function (done) {
+    gulp
+        .src('./sass/style.scss', { sourcemaps: true })
+        .pipe(sourcemaps.init())
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions']
+            })
+        )
+        .pipe(gulp.dest('./build/css'))
+        .pipe(uglifycss())
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./build/css'));
+
+    done();
+});
 
 
 gulp.task('reload', function () {
@@ -32,6 +60,26 @@ gulp.task('reload', function () {
 
 gulp.task("watch", function () {
     gulp.watch("./js/*.js", gulp.series("scripts"));
+});
+
+// Set-up BrowserSync and watch for sass
+gulp.task('browser-sync', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp
+        .watch('build/css/*.css')
+        .on('change', browserSync.reload);
+
+    done();
+});
+
+gulp.task('watch', function (done) {
+    gulp.watch('sass/*.scss', gulp.series('sass'));
+    done();
 });
 
 
--- js/script.js
+++ js/script.js
@@ -3,9 +3,25 @@ $(function () {
     const $select = $('select');
 
     $select.on('change', function (event) {
-        console.log($select.val());
+        console.log("event ", event.target.value);
+        // console.log($select.val());
+        $.ajax({
+            method: "GET",
+            url: "https://api.nytimes.com/svc/topstories/v2/" + event.target.value + ".json?api-key=pKVd06Mh3sAKyIaCdi5xzTGMW6D5w17t"
+        }).done(function (data) {
+            console.log("data", data);
+            // // $(".results").append("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'/>");
+            // $(".results").append(`<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`);
+            // $(".results").append(`<p>${data.weather[0].main}</p>`);
+            // $(".results").append(`<p>${data.weather[0].description}</p>`);
+
+
+
+
+        });
 
     });
+
 
 
 });
