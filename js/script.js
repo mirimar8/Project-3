f49f099c2b97ef5f71c29ab41a9ff93eb8a74134
Miri <mirimarkovitzbl@gmail.com>
2019-06-11 18:44:27 -0700

    add css animation and remove extraneous code and comments.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,11 @@ $(function () {
 
     const $select = $('select');
     const $gridContainer = $('.grid-container');
-    // const $multimedia = $('data.results[index].multimedia');
+    const $ajaxLoader = $('.ajax-loader');
 
 
     $select.on('change', function (event) {
-        console.log("event", event.target.value);
-        $(".ajax-loader").show();
+        $ajaxLoader.show();
 
         $(".logo").addClass("logo-on-click");
         $(".top-header").addClass("top-header-on-click");
@@ -17,23 +16,22 @@ $(function () {
 
         $gridContainer.text('');
 
-        // console.log($select.val());
         $.ajax({
             method: "GET",
             url: "https://api.nytimes.com/svc/topstories/v2/" + event.target.value + ".json?api-key=pKVd06Mh3sAKyIaCdi5xzTGMW6D5w17t"
         }).done(function (data) {
-            console.log("data", data);
-            // console.log(data.results[0].multimedia[4].url);
-            $(".ajax-loader").hide();
+            $ajaxLoader.hide();
 
 
             let count = 0;
             data.results.forEach(function (element, index) {
-                if (count < 12 && data.results[index].multimedia.length >= 5) {
+                const numStories = data.results[index].multimedia.length;
+
+                if (count < 12 && numStories >= 5) {
                     count += 1;
                     $gridContainer.append(`<li>
                         <a href="${data.results[index].url}" target="_blank">
-                            <div style="background-image: url(${data.results[index].multimedia[4].url})">
+                            <div class="story-image" style="background-image: url(${data.results[index].multimedia[4].url})">
                                 <p>${data.results[index].abstract}</p>
                             </div>
                         </a>
