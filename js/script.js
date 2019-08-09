392affe99c51d5adfb5ddab15db924a33347cce3
Miri <mirimarkovitzbl@gmail.com>
2019-08-08 19:12:12 -0700

    fix some errors in the js file.

--- js/script.js
+++ js/script.js
@@ -8,19 +8,18 @@ $(function () {
     $select.on('change', function (event) {
         $ajaxLoader.show();
 
-        $(".logo").addClass("logo-on-click");
-        $(".top-header").addClass("top-header-on-click");
-        $(".choose-section").addClass("choose-section-on-click");
-        $(".choose-section-wrapper").addClass("choose-section-wrapper-on-click");
-        $(".copyright").addClass("copyright-on-click");
+        $('.logo').addClass('logo-on-click');
+        $('.top-header').addClass('top-header-on-click');
+        $('.choose-section').addClass('choose-section-on-click');
+        $('.choose-section-wrapper').addClass('choose-section-wrapper-on-click');
+        $('.copyright').addClass('copyright-on-click');
         $gridContainer.text('');
 
         $.ajax({
-            method: "GET",
-            url: "https://api.nytimes.com/svc/topstories/v2/" + event.target.value + ".json?api-key=pKVd06Mh3sAKyIaCdi5xzTGMW6D5w17t"
+            method: 'GET',
+            url: 'https://api.nytimes.com/svc/topstories/v2/' + event.target.value + '.json?api-key=pKVd06Mh3sAKyIaCdi5xzTGMW6D5w17t'
         }).done(function (data) {
             $ajaxLoader.hide();
-
 
             let count = 0;
             data.results.forEach(function (element, index) {
@@ -29,19 +28,14 @@ $(function () {
                 if (count < 12 && numStories >= 5) {
                     count += 1;
                     $gridContainer.append(`<li>
-                        <a href="${data.results[index].url}" target="_blank">
-                            <div class="story-image" style="background-image: url(${data.results[index].multimedia[4].url})">
+                        <a href='${data.results[index].url}' target='_blank'>
+                            <div class='story-image' style='background-image: url(${data.results[index].multimedia[4].url})'>
                                 <p>${data.results[index].abstract}</p>
                             </div>
                         </a>
                     </li>`);
-
-                };
-
+                }
             });
-
         });
-
     });
-
 });
